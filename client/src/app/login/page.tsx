dbf9598735b01d65386e8956a12f5f30a3145198
Juan Vasquez <juancamilovasquezj@gmail.com>
2023-11-25 10:45:18 +0000

    init Next

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function Login() {
+  return (
+    <>
+      <div>
+        <h1></h1>
+        <p></p>
+        <div></div>
+      </div>
+      <div>
+        <div className='stats'></div>
+        <div className='stats'></div>
+        <div className='stats'></div>
+        <Image src='/girl-homepage.png' alt='Home Image' width='774' height='1000'></Image>
+      </div>
+    </>
+  );
+}

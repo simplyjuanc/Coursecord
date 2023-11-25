66be15184903b8f557084f10dbe2d2a6857023a7
Bruno Pastorelli <brunopastorelli12@gmail.com>
2023-11-25 16:01:25 +0000

    ClassList Done.

---
+++ client/src/app/student-classes/components/ClassList.tsx
@@ -0,0 +1,46 @@
+// CourseList.tsx
+"use client";
+import React, { useState, useEffect } from 'react';
+
+interface CourseInfo {
+  id: string;
+  imageUrl: string; // Assuming we only need the ID and the image URL for the CourseList
+}
+
+const CourseList: React.FC = () => {
+  const [courses, setCourses] = useState<CourseInfo[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchCourses() {
+      try {
+        const response = await fetch('/api/courses'); // Adjust this to your actual API endpoint
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        setCourses(data);
+      } catch (e) {
+        setError(e instanceof Error ? e.message : 'An unknown error occurred');
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchCourses();
+  }, []);
+
+  if (loading) return <div>Loading courses...</div>;
+  if (error) return <div>Error fetching courses: {error}</div>;
+
+  return (
+    <div className="flex overflow-x-auto py-4 space-x-4">
+      {courses.map((course) => (
+        <img key={course.id} src={course.imageUrl} alt="Course" className="h-auto md:h-48" /> // Adjust the height as needed
+      ))}
+    </div>
+  );
+};
+
+export default CourseList;

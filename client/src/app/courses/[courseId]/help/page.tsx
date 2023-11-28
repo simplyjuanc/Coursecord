c983c3f4c28881b5b83c835523c316606ed237f3
Arthur Jameson <arthurj17@gmail.com>
2023-11-28 18:29:10 +0000

    feat: style help request (#44)

--- client/src/app/courses/[courseId]/help/page.tsx
+++ client/src/app/courses/[courseId]/help/page.tsx
@@ -1,5 +1,28 @@
+import { MdOutlineSupportAgent } from 'react-icons/md';
+
 export default function Help() {
-    return <div>
-        help!
-    </div>
+  return (
+    <section className='h-screen flex items-center'>
+      <div className='mx-auto w-3/5 h-2/3 bg-white shadow-lg border-solid border-2 border-grey-600 rounded-lg p- flex flex-col items-center justify-evenly'>
+        <div className='flex items-center text-4xl font-bold text-center drop-shadow-lg'>
+          <MdOutlineSupportAgent />
+          <h2>
+            Help Request
+          </h2>
+        </div>
+        <div className='h-1/3 p-4'>
+          <label htmlFor='input-description'>
+            Anything else you want us to know?
+          </label>
+          <textarea
+            id='input-description'
+            className='p-1 border-2 border-solid border-slate resize-none h-full w-full rounded-md'
+          />
+        </div>
+        <button className='my-5 mx-auto w-1/4 p-2 bg-red-400 border-2 border-solid border-grey rounded-lg shadow-md text-gray-900 font-bold hover:bg-red-500'>
+          Send
+        </button>
+      </div>
+    </section>
+  );
 }
--- client/src/app/courses/[courseId]/layout.tsx
+++ client/src/app/courses/[courseId]/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="">{children}</div>
+      <div className="w-full">{children}</div>
     </div>
   );
 }

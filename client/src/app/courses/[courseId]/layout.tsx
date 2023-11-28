f494f3d46825f6d14a349d1a4fe5b3cb2859656b
Alex <alexander.j.heather@gmail.com>
2023-11-28 16:47:45 +0000

    fix: sidebar

--- client/src/app/courses/[courseId]/layout.tsx
+++ client/src/app/courses/[courseId]/layout.tsx
@@ -2,9 +2,9 @@ import Sidebar from "@/components/sidebar/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="grid grid-cols-7">
+    <div className="flex">
       <Sidebar />
-      <div className="col-span-6">{children}</div>
+      <div className="">{children}</div>
     </div>
   );
 }

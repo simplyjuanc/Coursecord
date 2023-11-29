79a4b60cff3fcf359967bc6e4b30aab578b12db1
Alex <alexander.j.heather@gmail.com>
2023-11-29 17:00:27 +0000

    fix: merge issue

--- client/src/app/courses/[courseId]/help/page.tsx
+++ client/src/app/courses/[courseId]/help/page.tsx
@@ -1,48 +1,19 @@
-  'use client';
-  import { useState } from 'react';
-  import HelpRequestForm from './HelpRequestForm';
-  import HelpRequestModal from './HelpRequestConfirmation';
+"use client";
+import { useState } from "react";
+import HelpRequestForm from "./HelpRequestForm";
+import HelpRequestModal from "./HelpRequestConfirmation";
 
-<<<<<<< HEAD
-export default function Help() {
+const HelpRequest = () => {
+  const [submitted, setSubmitted] = useState(false);
   return (
-    <section className='h-screen w-full flex items-center'>
-      <div className='mx-auto min-w-max w-1/3 aspect-square bg-white shadow-lg border-solid border-2 border-grey-600 rounded-lg flex flex-col items-center justify-evenly'>
-        <div className='flex items-center text-4xl font-bold text-center drop-shadow-lg'>
-          <MdOutlineSupportAgent />
-          <h2>
-            Help Request
-          </h2>
-        </div>
-        <div className='h-1/3 p-4'>
-          <label htmlFor='input-description'>
-            Anything else you want us to know?
-          </label>
-          <textarea
-            id='input-description'
-            className='p-1 border-2 border-solid border-slate resize-none h-full w-full rounded-md'
-          />
-        </div>
-        <button className='my-5 mx-auto w-1/4 p-2 bg-red-400 border-2 border-solid border-grey rounded-lg shadow-md text-gray-900 font-bold hover:bg-red-500'>
-          Send
-        </button>
-      </div>
-    </section>
+    <>
+      {submitted ? (
+        <HelpRequestModal />
+      ) : (
+        <HelpRequestForm setSubmitted={setSubmitted} />
+      )}
+    </>
   );
-}
-=======
-  const HelpRequest = () => {
-    const [submitted, setSubmitted] = useState(false);
-    return (
-      <>
-        {submitted ? (
-          <HelpRequestModal />
-        ) : (
-          <HelpRequestForm setSubmitted={setSubmitted} />
-        )}
-      </>
-    );
-  };
+};
 
-  export default HelpRequest;
->>>>>>> dev
+export default HelpRequest;

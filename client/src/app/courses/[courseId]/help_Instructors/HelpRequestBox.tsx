e3921d74fba71792f7eb747bf69f74293ade9b37
Bruno Pastorelli <brunopastorelli12@gmail.com>
2023-11-29 14:15:21 +0000

    instructors help request 1.2

--- client/src/app/courses/[courseId]/help_Instructors/HelpRequestBox.tsx
+++ client/src/app/courses/[courseId]/help_Instructors/HelpRequestBox.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-// HelpRequestBox
-
 import React from 'react';
 
 interface HelpRequestBoxProps {
@@ -12,11 +10,13 @@ interface HelpRequestBoxProps {
 
 const HelpRequestBox: React.FC<HelpRequestBoxProps> = ({ id, studentName, question }) => {
   return (
-    <div className="border p-4 rounded shadow-sm bg-white my-2">
-      <h4 className="font-bold">{studentName}</h4>
-      <p>{question}</p>
+    <div className="border-2 border-gray-300 bg-white rounded-lg shadow p-4 my-2 flex flex-col">
+      <div>
+        <h4 className="text-lg font-bold text-gray-900">{studentName}</h4>
+        <p className="text-gray-600">{question}</p>
+      </div>
     </div>
   );
 };
 
-export default HelpRequestBox;
+export default HelpRequestBox
--- client/src/app/courses/[courseId]/help_Instructors/page.tsx
+++ client/src/app/courses/[courseId]/help_Instructors/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 
-// InstructorHelpRequestPage.tsx
-
 import React, { useState, useEffect } from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import io from 'socket.io-client';
 import HelpRequestBox from './HelpRequestBox';
+import HelpRequestHeader from "./imgs/headerHelp.png";
+import Image from 'next/image'
+
 
 interface HelpRequest {
   id: string;
   studentName: string;
   question: string;
+  status: 'waiting' | 'assigned' | 'completed';
 }
 
 const InstructorHelpRequestPage = () => {
@@ -18,59 +20,91 @@ const InstructorHelpRequestPage = () => {
   const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:5001');
 
   useEffect(() => {
-    // The event name here should match the one used by the server when a new help request is created
-    socket.on('requestCreated', (newRequest: HelpRequest) => {
-      // Update the state to include the new help request
-      setHelpRequests((prevRequests) => [...prevRequests, newRequest]);
+    socket.on('requestCreated', (newRequest: Omit<HelpRequest, 'status'>) => {
+      setHelpRequests((prevRequests) => [
+        ...prevRequests,
+        { ...newRequest, status: 'waiting' }, // Default new requests to waiting
+      ]);
     });
 
-    // Clean up the event listener when the component unmounts
     return () => {
       socket.off('requestCreated');
       socket.disconnect();
     };
   }, []);
 
-  // Handler for drag and drop events
   const onDragEnd = (result: any) => {
     if (!result.destination) return;
 
     const items = Array.from(helpRequests);
     const [reorderedItem] = items.splice(result.source.index, 1);
+    reorderedItem.status = result.destination.droppableId as 'waiting' | 'assigned' | 'completed';
     items.splice(result.destination.index, 0, reorderedItem);
 
     setHelpRequests(items);
   };
 
-  // Render the help requests within a draggable context
+  const renderHelpRequests = (status: 'waiting' | 'assigned' | 'completed') => {
+    return helpRequests
+      .filter((req) => req.status === status)
+      .map((request, index) => (
+        <Draggable key={request.id} draggableId={request.id} index={index}>
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.draggableProps}
+              {...provided.dragHandleProps}
+              className="mb-2"
+            >
+              <HelpRequestBox
+                id={request.id}
+                studentName={request.studentName}
+                question={request.question}
+              />
+            </div>
+          )}
+        </Draggable>
+      ));
+  };
+
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Droppable droppableId="helpRequests">
-        {(provided) => (
-          <div ref={provided.innerRef} {...provided.droppableProps} className="space-y-4">
-            {helpRequests.map((request, index) => (
-              <Draggable key={request.id} draggableId={request.id} index={index}>
-                {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    className="mb-2"
-                  >
-                    <HelpRequestBox
-                      id={request.id}
-                      studentName={request.studentName}
-                      question={request.question}
-                    />
+    <>
+      <div className="flex justify-center my-4">
+        <div className="w-80">
+          <Image 
+            src={HelpRequestHeader}
+            alt="Help Request Header"
+            width={256} 
+            height={80} 
+            layout="responsive" 
+          />
+        </div>
+      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <div className="flex justify-around p-4 bg-gray-100" style={{ height: 'calc(100vh - 4rem)' }}>
+          {['waiting', 'assigned', 'completed'].map((status) => (
+            <Droppable key={status} droppableId={status}>
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className={`w-1/4 mx-2 mt-7 p-4 rounded-lg border shadow-sm ${
+                    snapshot.isDraggingOver ? 'border-blue-500 bg-blue-50' : 'border-gray-300 bg-white'
+                  }`}
+                  style={{ minHeight: '50vh' }}
+                >
+                  <h3 className="text-xl font-bold capitalize mb-4">{status}</h3>
+                  <div className="flex flex-col space-y-4 overflow-auto">
+                    {renderHelpRequests(status as 'waiting' | 'assigned' | 'completed')}
+                    {provided.placeholder}
                   </div>
-                )}
-              </Draggable>
-            ))}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-    </DragDropContext>
+                </div>
+              )}
+            </Droppable>
+          ))}
+        </div>
+      </DragDropContext>
+    </>
   );
 };
 

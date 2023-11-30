b6fb6a39ad0965f3d9856eabd1fd8696fc8befe2
Juan Vasquez <juancamilovasquezj@gmail.com>
2023-11-30 00:13:32 +0000

    style: add grid structure to AdminTable

--- client/src/app/courses/[courseId]/admin/components/AddNewUser.tsx
+++ client/src/app/courses/[courseId]/admin/components/AddNewUser.tsx
@@ -5,13 +5,20 @@ import IconButton from '@/components/buttons/iconButton';
 import { MdOutlinePersonAddAlt } from 'react-icons/md';
 
 type AddUserProps = {
-  courseId: string
-  setShowNewUser: React.Dispatch<React.SetStateAction<boolean>>
-  roles: Role[]
+  courseId: string;
+  setShowNewUser: React.Dispatch<React.SetStateAction<boolean>>;
+  roles: Role[];
 };
 
-export default function AddNewUser({ setShowNewUser, courseId, roles }: AddUserProps) {
-  const [partialFormState, setPartialFormState] = useState({ name: '', email: ''});
+export default function AddNewUser({
+  setShowNewUser,
+  courseId, // see TODO below
+  roles,
+}: AddUserProps) {
+  const [partialFormState, setPartialFormState] = useState({
+    name: '',
+    email: '',
+  });
   const [selectedRole, setSelectedRole] = useState<Role>();
 
   useEffect(() => {
@@ -22,14 +29,13 @@ export default function AddNewUser({ setShowNewUser, courseId, roles }: AddUserP
     return () => window.removeEventListener('keydown', handleKeyDown);
   }, [setShowNewUser]);
 
-
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     setPartialFormState({
       ...partialFormState,
       [e.target.name]: e.target.value,
     });
   }
-  
+
   function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     if (!partialFormState.name || !partialFormState.email || !selectedRole) {
@@ -38,7 +44,7 @@ export default function AddNewUser({ setShowNewUser, courseId, roles }: AddUserP
       // TODO: Fetch request to add new user to course, looking for their email or their name
       // TODO: Add new user to the course (need to create addUser route and controller in backend)
       closeModal();
-    };
+    }
   }
 
   function closeModal() {
@@ -47,54 +53,58 @@ export default function AddNewUser({ setShowNewUser, courseId, roles }: AddUserP
 
   return (
     <div className='absolute top-0 left-0 flex items-center justify-center w-full h-full bg-opacity-50 bg-slate-100'>
-      <div className='fixed w-1/3 p-8 bg-white rounded-lg shadow-lg h-1/3'>
-        <div>
-          <div
-            className='absolute text-xl cursor-pointer right-6 top-4'
-            onClick={closeModal}
-          >
-            X
+      <div className='fixed w-1/3 p-8 bg-white rounded-lg shadow-lg'>
+        <div
+          className='absolute text-xl cursor-pointer right-6 top-4'
+          onClick={closeModal}
+        >
+          X
+        </div>
+        <h1 className='mx-auto mt-2 mb-6 text-2xl font-semibold text-center'>
+          Add New User
+        </h1>
+
+        <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
+          <div className='flex flex-row justify-between mx-12'>
+            <label htmlFor='name' className='text-xl font-semibold'>
+              Name
+            </label>
+            <input
+              name='name'
+              onChange={handleInputChange}
+              type='text'
+              placeholder='Kynncer Doe'
+              className='px-5 ml-auto text-right border rounded-md'
+            />
           </div>
-          <h1 className='mx-auto mt-2 mb-6 text-2xl font-semibold text-center'>
-            Add New User
-          </h1>
 
-          <form onSubmit={handleSubmit} className='flex flex-col gap-6'>
-            <div className='flex flex-row justify-between mx-12'>
-              <label htmlFor='name' className='text-xl font-semibold'>Name</label>
-              <input
-                name='name'
-                onChange={handleInputChange}
-                type='text'
-                placeholder='Kynncer Doe'
-                className='px-5 ml-auto text-right border rounded-md'
-              />
-            </div>
+          <div className='flex flex-row justify-between mx-12'>
+            <label htmlFor='email' className='text-xl font-semibold'>
+              Email
+            </label>
+            <input
+              name='email'
+              onChange={handleInputChange}
+              type='email'
+              placeholder='required.madness@gmail.com'
+              className='px-5 ml-auto text-right border rounded-md'
+            />
+          </div>
 
-            <div className='flex flex-row justify-between mx-12'>
-              <label htmlFor='email' className='text-xl font-semibold'>Email</label>
-              <input
-                name='email'
-                onChange={handleInputChange}
-                type='email'
-                placeholder='required.madness@gmail.com'
-                className='px-5 ml-auto text-right border rounded-md'
-              />
-            </div>
-
-            <div className='flex flex-row justify-between mx-12'>
-              <label htmlFor='role' className='text-xl font-semibold'>Role</label>
-              <RoleSelect roles={roles} setRole={setSelectedRole} />
-            </div>
-            <div className='w-1/3 py-3 mx-auto'>
+          <div className='flex flex-row justify-between mx-12'>
+            <label htmlFor='role' className='text-xl font-semibold'>
+              Role
+            </label>
+            <RoleSelect roles={roles} setRole={setSelectedRole} />
+          </div>
+          <div className='w-1/3 py-3 mx-auto'>
             <IconButton
               icon={<MdOutlinePersonAddAlt />}
               title='Submit'
               onClick={handleSubmit}
-              ></IconButton>
-            </div>
-          </form>
-        </div>
+            ></IconButton>
+          </div>
+        </form>
       </div>
     </div>
   );
--- client/src/app/courses/[courseId]/admin/page.tsx
+++ client/src/app/courses/[courseId]/admin/page.tsx
@@ -1,10 +1,14 @@
 'use client';
-import IconButton from '@/components/buttons/iconButton';
-import { useParams } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
-import { MdOutlinePersonAddAlt } from 'react-icons/md';
-import AddNewUser from './components/AddNewUser';
+import { useParams } from 'next/navigation';
+import {
+  MdOutlinePersonAddAlt,
+  MdOutlineModeEdit,
+  MdOutlineRestoreFromTrash,
+} from 'react-icons/md';
+import IconButton from '@/components/buttons/iconButton';
 import { Course, DbUser, Role } from '@/@types';
+import AddNewUser from './components/AddNewUser';
 import AddExistingUser from './components/AddExistingUser';
 
 export default function AdminTable() {
@@ -55,8 +59,8 @@ export default function AdminTable() {
   }
 
   // NEXT:
-  // Add button to remove user from course
-  // Add button to change user role
+  // Add edit and delete functinality for each user
+  // After that, style the table
 
   return (
     <section className='mt-12'>
@@ -77,9 +81,9 @@ export default function AdminTable() {
       </div>
 
       {showNewUser && (
-        <AddNewUser 
-          courseId={courseId} 
-          setShowNewUser={setShowNewUser} 
+        <AddNewUser
+          courseId={courseId}
+          setShowNewUser={setShowNewUser}
           roles={roles}
         />
       )}
@@ -91,25 +95,65 @@ export default function AdminTable() {
         />
       )}
 
-      {(course?.instructors || course?.students) && (
-        <div className='grid w-1/2 grid-cols-3 p-8 mx-auto gap-y-1'>
-          <div className='mb-2'>Name</div>
-          <div className='mb-2'>Email</div>
-          <div className='mb-2'>Role</div>
-          {instructors && instructors.map((instructor) => UserRow(instructor))}
-          {students && students.map((student) => UserRow(student))}
+      {!(course?.instructors || course?.students) ? (
+        <p>No users currently assigned!</p>
+      ) : (
+        <div className='grid grid-cols-7 p-8 mx-auto'>
+          <div className='col-span-1 py-1 mb-2 text-lg font-semibold text-center bg-opacity-25 rounded-tl-lg border-1 text bg-primary-red border-primary-gray'>
+            Actions
+          </div>
+          <div className='col-span-2 py-1 mb-2 text-lg font-semibold text-center bg-opacity-25 border-1 text bg-primary-red border-primary-gray'>
+            Name
+          </div>
+          <div className='col-span-2 py-1 mb-2 text-lg font-semibold text-center bg-opacity-25 border-1 text bg-primary-red border-primary-gray'>
+            Email
+          </div>
+          <div className='col-span-2 py-1 mb-2 text-lg font-semibold text-center bg-opacity-25 rounded-tr-lg border-1 text bg-primary-red border-primary-gray'>
+            Role
+          </div>
+          {instructors &&
+            instructors.map((instructor) => UserRow(instructor, roles))}
+          {students && students.map((student) => UserRow(student, roles))}
         </div>
       )}
     </section>
   );
 }
 
-function UserRow(user: DbUser): React.JSX.Element {
+function UserRow(user: DbUser, roles: Role[]): React.JSX.Element {
+  const userRoles = user.roles.map((id) => {
+    const userRole = roles.find((role) => role.id === id);
+    if (!userRole) return;
+    return userRole.title[0].toUpperCase() + userRole.title.slice(1);
+  });
+
+  function handleEdit() {
+    console.log('edit');
+  }
+
+  function handleDelete() {
+    console.log('delete');
+  }
+
   return (
-    <div key={user.id} className='grid grid-cols-3 col-span-3'>
-      <div>{user.name}</div>
-      <div>{user.email}</div>
-      <div>{user.roles.join(', ')}</div>
-    </div>
+    // <div key={user.id} className='grid grid-cols-4 col-span-4'>
+    <>
+      <div className='flex flex-row justify-center col-span-1 gap-2 py-2'>
+        <div className='cursor-pointer' onClick={handleEdit}>
+          <MdOutlineModeEdit />{' '}
+        </div>
+        <div className='cursor-pointer' onClick={handleDelete}>
+          <MdOutlineRestoreFromTrash />
+        </div>
+      </div>
+      <div className='col-span-2 py-2 text-center'>{user.name}</div>
+      <div className='col-span-2 py-2 text-center'>{user.email}</div>
+      <div className='col-span-2 py-2 text-center'>
+        {userRoles && userRoles.join(', ')}
+      </div>
+    </>
   );
+  {
+    /* </div> */
+  }
 }

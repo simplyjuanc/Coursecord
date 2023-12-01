59d9be6844bd89ad36add629adfad43578a79e08
Bruno Pastorelli <brunopastorelli12@gmail.com>
2023-12-01 11:03:03 +0000

    skip

--- client/src/app/courses/[courseId]/dashboard/page.tsx
+++ client/src/app/courses/[courseId]/dashboard/page.tsx
@@ -3,6 +3,8 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation'; 
 import type { NextPage } from 'next';
+import { useAppSelector } from '@/store';
+import { stat } from 'fs';
 
 interface Course {
   id: string;
@@ -23,56 +25,23 @@ interface Organisation {
 
 const Dashboard: NextPage = () => {
   const router = useRouter();
-  const [searchParams] = useSearchParams();
-const courseId = searchParams ? searchParams.get('courseId') : null;
-
-  const [courseDetails, setCourseDetails] = useState<Course | null>(null);
-  const [organisation, setOrganisation] = useState<Organisation | null>(null);
-  const [students, setStudents] = useState<User[]>([]);
-  const [instructors, setInstructors] = useState<User[]>([]);
-
-  const fetchData = async () => {
-    if (!courseId) return;
-    try {
-      const courseRes = await fetch(`/api/course/${courseId}`);
-      const courseData: Course = await courseRes.json();
-      setCourseDetails(courseData);
-
-      const orgRes = await fetch(`/api/organisation/${courseData.organisation}`);
-      const orgData: Organisation = await orgRes.json();
-      setOrganisation(orgData);
-
-      const studentsRes = await fetch(`/api/${courseId}/students`);
-      const studentsData: User[] = await studentsRes.json();
-      setStudents(studentsData);
-
-      const instructorsRes = await fetch(`/api/${courseId}/instructors`);
-      const instructorsData: User[] = await instructorsRes.json();
-      setInstructors(instructorsData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, [courseId]);
+  const courseInfo = useAppSelector(state => state.course.courseInfo);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold">Course Dashboard</h1>
-      {courseDetails && (
+      {courseInfo && (
         <div className="my-4">
-          <h2 className="text-xl font-semibold">{courseDetails.title}</h2>
-          <p>{courseDetails.description}</p>
-          {organisation && <p>Organisation: {organisation.name}</p>}
+          <h2 className="text-xl font-semibold">{courseInfo.title}</h2>
+          <p>{courseInfo.description}</p>
+          <p>Organisation: {courseInfo.organisation}</p>
         </div>
       )}
 
       <div className="my-4">
         <h2 className="text-lg font-semibold">Students</h2>
         <ul>
-          {students.map(student => (
+          {courseInfo.students.map(student => (
             <li key={student.id} className="list-disc list-inside">{student.name}</li>
           ))}
         </ul>
@@ -81,7 +50,7 @@ const courseId = searchParams ? searchParams.get('courseId') : null;
       <div className="my-4">
         <h2 className="text-lg font-semibold">Instructors</h2>
         <ul>
-          {instructors.map(instructor => (
+          {courseInfo.instructors.map(instructor => (
             <li key={instructor.id} className="list-disc list-inside">{instructor.name}</li>
           ))}
         </ul>

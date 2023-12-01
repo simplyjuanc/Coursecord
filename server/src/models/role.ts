5525524ff3b004649356e90d49a71daaa96818e8
arthurjamesoncode <arthurj17@gmail.com>
2023-12-01 16:22:00 +0000

    refactor role model

--- server/src/models/role.ts
+++ server/src/models/role.ts
@@ -1,50 +1,15 @@
 import { Role } from './index';
-import Organisation from './organisation';
-import User from '../models/user';
-
-
-async function createDefaultRoles() {
-  const newRoles = [];
-  newRoles.push(
-    (await Role.create({ data: { title: "admin", permissions: ["admin"] } })).id
-  );
-  newRoles.push(
-    (
-      await Role.create({
-        data: { title: "instructor", permissions: ["instructor"] },
-      })
-    ).id
-  );
-  newRoles.push(
-    (
-      await Role.create({
-        data: { title: "student", permissions: ["student"] },
-      })
-    ).id
-  );
-
-  return newRoles;
-}
 
 async function userRolesIncludes(
   userRoles: string[],
   title: string,
   orgId: string
 ) {
-  const org = await Organisation.getOrganisationById(orgId);
+  const roles = await Role.findMany({
+    where: { id: { in: userRoles }, organisation_id: orgId, title: title },
+  });
 
-  if (!org) {
-    throw new Error("Organisation Not Found");
-  }
-
-  const orgRoles = await Role.findMany({ where: { id: { in: org.roles } } });
-  for (const role of orgRoles) {
-    if (role.title === title && userRoles.includes(role.id)) {
-      return true;
-    }
-  }
-
-  return false;
+  return roles.length > 0;
 }
 
 async function getRoleByTitle(orgRoles: string[], title: string) {
@@ -71,36 +36,17 @@ async function deleteRolesInOrg(orgRoles: string[]) {
 }
 
 async function getRolesByOrg(orgId: string) {
-  const org = await Organisation.getOrganisationById(orgId);
-  if (!org) throw new Error('Organisation Not Found');
-  const roles = await Role.findMany({ where: { id: { in: org.roles } } });
+  const roles = await Role.findMany({
+    where: { organisation_id: orgId },
+  });
+
   return roles;
 }
 
 async function getRolesByUser(userId: string) {
-  const userPromise = User.getUserById(userId);
-  const orgsWithUserPromise = Organisation.getOrganisationsWithMember(userId);
-  const [user, orgsWithUser] = await Promise.all([
-    userPromise,
-    orgsWithUserPromise,
-  ]);
-  if (!user || !orgsWithUser) throw new Error("User or Organisation Not Found");
-
-  const rolePromises = user.roles.map(async (roleId) => {
-    let organisation = "";
-    for (const org of orgsWithUser) {
-      if (org.roles.includes(roleId)) {
-        organisation = org.id;
-        break;
-      }
-    }
-    return {
-      ...(await getRoleById(roleId.replace("\n", ""))),
-      organisation,
-    };
+  const roles = Role.findMany({
+    where: { users: { some: { user_id: userId } } },
   });
-
-  const roles = await Promise.all(rolePromises);
 
   return roles;
 }
@@ -111,5 +57,6 @@ export default {
   getRoleByTitle,
   deleteRolesInOrg,
   getRolesByUser,
-  getRolesByOrg
+  getRolesByOrg,
+  getRoleById,
 };

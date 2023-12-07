4ad196d205dc5e4b6e0abc11c219426441cd0e32
Arthur Jameson <arthurj17@gmail.com>
2023-12-07 10:37:41 +0000

    106 fe fix styling for courses page (#133)
    * chore: change favicon
    
    * added styling to management page and fixed course select
    
    * fast
    
    * yep
    
    ---------
    
    Co-authored-by: Juan Vasquez <juancamilovasquezj@gmail.com>

--- client/src/components/helpRequests/HelpRequestConfirmation.tsx
+++ client/src/components/helpRequests/HelpRequestConfirmation.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function HelpRequestModal() {
   return (
-    <div className="flex items-center min-h-full w-full">
-    <div className="text-center py-6 px-10 border border-gray-200 shadow-lg rounded-lg m-auto bg-white">
+    <div className="flex items-center min-h-full w-full bg-white">
+    <div className="text-center py-6 px-10 border border-primary-2 border-opacity-70 shadow-lg rounded-lg m-auto bg-white">
       <h1 className="text-lg font-bold">Help is on the way!</h1>
     </div>
   </div>

3306a9101d5ce725e0dced48ccc773660e7f1d0e
arthurjamesoncode <arthurj17@gmail.com>
2023-12-07 09:38:19 +0000

    added styling to management page and fixed course select

--- client/src/components/admin/CourseManagement.tsx
+++ client/src/components/admin/CourseManagement.tsx
@@ -1,8 +1,7 @@
 import { MdOutlinePersonAddAlt } from 'react-icons/md';
 import IconButton from '@/components/buttons/iconButton';
 import { useEffect, useState } from 'react';
-import AddExistingUser from './AddNewUser';
-import { Course, SessionWithToken, TCourseManagement, User } from '@/types';
+import { Course, SessionWithToken, User } from '@/types';
 import {
   deleteUserFromCourse,
   getCourseData,
@@ -15,7 +14,6 @@ import {
   addCourse,
   removeUserFromCourse,
 } from '@/store/slices/managementSlice';
-import { set } from 'date-fns';
 
 type CourseManagementProps = {
   course: {
@@ -50,8 +48,10 @@ export default function CourseManagement(props: CourseManagementProps) {
         }
         console.log('updatedCourse :>> ', updatedCourse);
         dispatch(addCourse(updatedCourse));
-        setActiveCourse(updatedCourse);
+        return setActiveCourse(updatedCourse);
       }
+
+      setActiveCourse(courses[course.id]);
     })();
   }, [course, courses, dispatch, session]);
 
@@ -77,7 +77,7 @@ export default function CourseManagement(props: CourseManagementProps) {
   console.log('activeCourse :>> ', activeCourse);
 
   return (
-    <div className='flex flex-col flex-none w-full max-h-full min-h-full p-6 mb-8 overflow-auto bg-white border-2 shadow-xl rounded-xl border-primary-gray border-opacity-40 overscroll-contain scrollbar-hide'>
+    <div className='flex flex-col w-full max-h-full min-h-full p-6 mb-8 overflow-auto bg-white border-2 shadow-xl rounded-xl border-primary-gray border-opacity-40 overscroll-contain scrollbar-hide'>
       <h1 className='mx-4 mb-6'>
         <span className='text-xl font-medium'>Course: </span>
         <span className='my-auto text-2xl font-semibold text-center drop-shadow-sm text-primary-1 text-opacity-70'>
@@ -88,8 +88,8 @@ export default function CourseManagement(props: CourseManagementProps) {
       {activeCourse && (
         <div className='flex flex-row flex-nowrap'>
 
-          <div className='flex flex-col items-center flex-start'>
-            <div className='flex flex-row justify-around w-full flex-nowrap '>
+          <div className='flex flex-col items-center flex-start w-1/2'>
+            <div className='flex flex-row justify-around w-full flex-nowrap mb-4'>
               <h2 className='my-auto text-xl font-semibold'>Instructors:</h2>
               <div className=''>
                 <IconButton
@@ -110,8 +110,8 @@ export default function CourseManagement(props: CourseManagementProps) {
 
           </div>
 
-          <div className='flex flex-col items-center flex-start'>
-            <div className='flex flex-row justify-around w-full flex-nowrap '>
+          <div className='flex flex-col items-center flex-start w-1/2'>
+            <div className='flex flex-row justify-around w-full flex-nowrap mb-4'>
               <h2 className='my-auto text-xl font-semibold'>Students:</h2>
               <div className=''>
                 <IconButton
